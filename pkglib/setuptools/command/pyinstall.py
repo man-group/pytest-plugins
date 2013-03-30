1bdeb6907c31a6544abc3143ce49455e36101493
Edward Easton <edward.easton@pythonpro.co.uk>
2013-03-30 15:07:37 +0000

    Some more improvements and fixes for the dependency resolver. Back-tracking is now more intelligent in how it determines which upstream requirements to remove from the working graph. Added another mode to the pydot graph drawing where you can now draw requirements instead of distributions - this represents the underlying data directly and is more useful for debugging.  Continual refactoring and PEP8.

--- pkglib/setuptools/command/pyinstall.py
+++ pkglib/setuptools/command/pyinstall.py
@@ -18,12 +18,15 @@ class pyinstall(Command, CommandMixin):
 
     user_options = [
         ('dev', None, "Installs development packages if they are available"),
-        ('update', 'U', "Forces an update of packages and their dependencies, respecting source checkouts."),
+        ('update', 'U',
+         "Forces an update of packages and their dependencies, respecting "
+         "source checkouts."),
         ('third-party', 't', 'Update only: include third-party packages'),
         ("index-url=", "i", "base URL of Python Package Index"),
         ('source', 's', 'Update only: include only source checkouts'),
         ('eggs', 'e', 'Update only: include only eggs'),
-        ('everything', 'E', 'Update only: include all currently installed packages'),
+        ('everything', 'E',
+         'Update only: include all currently installed packages'),
     ]
     boolean_options = [
         'dev',
@@ -53,37 +56,43 @@ class pyinstall(Command, CommandMixin):
             return
 
         if not self.update:
-            # UNKNOWN distro name is set when this is run from pyinstall the command-line app.
+            # UNKNOWN distro name is set when this is run from pyinstall the 
+            # command-line app.
             if self.distribution.get_name() == 'UNKNOWN' and not self.args:
-                raise DistutilsOptionError('Please specify some packages requirements.')
+                raise DistutilsOptionError('Please specify some package '
+                                           'requirements.')
 
         if not self.update:
-            # If we're not in update mode, figure out whether or not what we've asked to
-            # install is already installed, and upgrade it instead - but only if it's a
-            # dev package.
+            # If we're not in update mode, figure out whether or not what we've
+            # asked to install is already installed, and upgrade it instead -
+            # but only if it's a  dev package.
             # This switch only works if all of the requirements
             # are being upgraded at once.
 
             reqs = list(pkg_resources.parse_requirements(self.args))
             req_names = [i.project_name for i in reqs]
-            installed_req_dists = [i for i in pkg_resources.working_set if i.project_name in req_names]
+            installed_req_dists = [i for i in pkg_resources.working_set
+                                   if i.project_name in req_names]
 
             if installed_req_dists:
-                # Some of our requirements are already installed. We swap to update mode
-                # but only if they're all being update at once
+                # Some of our requirements are already installed. 
+                # We swap to update mode but only if they're all being updated
+                # at once
                 if len(reqs) != len(installed_req_dists):
                     raise DistutilsOptionError('\n'.join([
                         'Cannot run an install and update at the same time.',
                         'Please specify less requirements.',
                         ]))
-                log.info("Requirements already installed, performing an update")
+                log.info("Requirements already installed, performing an "
+                         "update")
                 self.update = True
 
         if self.update and not self.dev:
             # We need to check if we're trying to update to a released version
-            # In this case we really should be doing an install instead as upgrading
-            # will most likely cause dependency errors.
-            log.info("Updating to a released version, performing an direct install.")
+            # In this case we really should be doing an install instead as
+            # upgrading will most likely cause dependency errors.
+            log.info("Updating to a released version, performing an direct "
+                     "install.")
             self.update = False
 
     def run(self):
@@ -91,17 +100,20 @@ class pyinstall(Command, CommandMixin):
         from pkglib.setuptools.buildout import install
 
         if not self.update:
-            install(self.get_easy_install_cmd(index_url=self.index_url), self.args, prefer_final=not self.dev,
+            install(self.get_easy_install_cmd(index_url=self.index_url),
+                    self.args, prefer_final=not self.dev,
                     force_upgrade=self.update)
         else:
             # Update mode - call the pyupdate command
             cls = self.distribution.get_command_class('update')
-            cmd = cls(self.distribution, dev=self.dev, everything=self.everything,
-                      third_party=self.third_party, source=self.source, eggs=self.eggs,
+            cmd = cls(self.distribution, dev=self.dev,
+                      everything=self.everything, third_party=self.third_party,
+                      source=self.source, eggs=self.eggs,
                       index_url=self.index_url)
             cmd.args = self.args
             cmd.ensure_finalized()
             cmd.no_cleanup = True
             cmd.run()
 
-        self.execute(self.run_cleanup_in_subprocess, (), 'Cleaning up site-packages')
+        self.execute(self.run_cleanup_in_subprocess, (),
+                     'Cleaning up site-packages')
