151c535e36ecb2cccb7a9f59df44ac95852867b0
Edward Easton <edward.easton@pythonpro.co.uk>
2013-07-03 17:28:51 +0200

    Added helper for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,12 @@
 """ Sphinx configuration is sourced from within pkgutils
 """
+import os
+import sys
 from pkglib.sphinx.conf import *
+if os.environ.get('READTHEDOCS', None) == 'True': 
+    os.chdir('..')
+    print os.getcwd()
+    sys.argv = ['setup.py', 'build_sphinx']
+    execfile('setup.py')
+
+    
