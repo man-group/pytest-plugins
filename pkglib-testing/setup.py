aeae96de4d989e9019cf61f58f82ea8b08bbc691
Wilfred Hughes <me@wilfred.me.uk>
2014-03-24 11:38:20 +0000

    Version bump now that 0.10.3 is tagged.

--- pkglib-testing/setup.py
+++ pkglib-testing/setup.py
@@ -81,7 +81,7 @@ def main():
         name='pkglib-testing',
         description='PkgLib testing library',
         long_description=long_description,
-        version='0.10.3',
+        version='0.10.4',
         # url='',
         license='MIT license',
         platforms=['unix', 'linux'],
