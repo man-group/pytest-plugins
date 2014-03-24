3a0822dfab0fc22ec8150295ce7b0bfb00802de5
Wilfred Hughes <me@wilfred.me.uk>
2014-03-24 14:38:46 +0000

    Fixing a six import to use pkglib.six.

--- pkglib/pkglib/pypi/pypirc.py
+++ pkglib/pkglib/pypi/pypirc.py
@@ -3,7 +3,7 @@ import stat
 import logging
 import time
 
-import six.moves.configparser as configparser
+from pkglib.six.moves import configparser
 from pkglib.six.moves.urllib.error import HTTPError
 from pkglib.six.moves.urllib.request import HTTPBasicAuthHandler, Request, build_opener
 from pkglib.six.moves.urllib.parse import urlencode
