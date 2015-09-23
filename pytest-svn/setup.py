b1db63b50abe75e7df26a469971b938f7b580032
jmatthews <jmatthews@ahl.com>
2015-09-23 15:10:41 +0100

    pytest-svn tidyup

--- pytest-svn/setup.py
+++ pytest-svn/setup.py
@@ -1,7 +1,7 @@
 import sys
 import logging
 
-from setuptools import setup, find_packages
+from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
 classifiers = [
@@ -20,6 +20,7 @@ classifiers = [
 long_description = open("README.rst").read()
 
 pytest_args = []
+
 
 class PyTest(TestCommand):
 
@@ -58,7 +59,7 @@ def main():
                         'pytest-shutil',
                         ]
 
-    tests_require = [
+    tests_require = ['pytest-cov',
                      ]
 
     entry_points = {
