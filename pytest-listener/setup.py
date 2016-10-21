38a25997e7b3bf05946e72d0a03d491198e36d59
Edward J M Easton <eeaston@ahl.com>
2016-10-21 14:38:14 +0100

    Dropping py26 support, sorry :(

--- pytest-listener/setup.py
+++ pytest-listener/setup.py
@@ -11,7 +11,6 @@ classifiers = [
     'Intended Audience :: Developers',
     'Operating System :: POSIX',
     'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.6',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
--- pytest-webdriver/setup.py
+++ pytest-webdriver/setup.py
@@ -10,7 +10,6 @@ classifiers = [
     'Intended Audience :: Developers',
     'Operating System :: POSIX',
     'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.6',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
