6896117ea6d049ff9afeba73bd733eac9422d59e
Xinghong Fang <xinghong.fang@man.com>
2018-12-10 17:37:54 +0000

    serverclass: update kill logic for thread server class

--- pytest-server-fixtures/pytest_server_fixtures/serverclass/thread.py
+++ pytest-server-fixtures/pytest_server_fixtures/serverclass/thread.py
@@ -18,16 +18,26 @@ from .common import ServerClass, is_debug
 log = logging.getLogger(__name__)
 
 
+# ThreadServer will attempt to kill all child processes recursively.
+KILL_RETRY_COUNT=15 # Total retry count to kill if not all child processes are terminated.
+KILL_RETRY_WAIT_SECS=1 # Wait time between two retries
+KILL_WAIT_SECS=5 # Time to wait for processes to terminate in a single retry.
+
+
 class ProcessStillRunningException(Exception):
     pass
 
 
 @retry(ProcessStillRunningException,
-       tries=15,
-       delay=1)
-def _wait_for_all(procs):
+       tries=KILL_RETRY_COUNT,
+       delay=KILL_RETRY_WAIT_SECS)
+def _kill_all(procs, sig):
+    log.debug("Killing %d processes with signal %s", len(procs), sig)
+    for p in procs:
+        p.send_signal(sig)
+
     log.debug("Waiting for %d processes to die", len(procs))
-    gone, alive = psutil.wait_procs(procs, timeout=1)
+    gone, alive = psutil.wait_procs(procs, timeout=KILL_WAIT_SECS)
 
     if len(alive) == 0:
         log.debug("All processes are terminated")
@@ -42,9 +52,7 @@ def _kill_proc_tree(pid, sig=signal.SIGKILL, timeout=None):
     children = parent.children(recursive=True)
     children.append(parent)
     log.debug("Killing process tree for %d (total_procs_to_kill=%d)", parent.pid, len(children))
-    for p in children:
-        p.send_signal(sig)
-    _wait_for_all(children)
+    _kill_all(children, sig)
 
 
 class ThreadServer(ServerClass):
@@ -61,7 +69,6 @@ class ThreadServer(ServerClass):
         self._workspace = workspace
         self._cwd = cwd
         self._proc = None
-        self._dead = False
         self._run_cmd = []
 
     def launch(self):
