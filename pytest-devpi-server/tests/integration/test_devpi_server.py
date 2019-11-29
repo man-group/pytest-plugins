1112a9f30ef8677f6d897ec77ff3bf1b3332d13d
Edward J. M. Easton <eeaston@gmail.com>
2019-11-29 15:52:35 +0000

    Black format changed files

--- pytest-devpi-server/tests/integration/test_devpi_server.py
+++ pytest-devpi-server/tests/integration/test_devpi_server.py
@@ -14,27 +14,34 @@ NEW_INDEX = {
 }
 
 
-
 def test_server(devpi_server):
-    res = devpi_server.api('getjson', '/{}/{}'.format(devpi_server.user, devpi_server.index))
+    res = devpi_server.api(
+        "getjson", "/{}/{}".format(devpi_server.user, devpi_server.index)
+    )
     assert json.loads(res) == NEW_INDEX
 
+
 def test_upload(devpi_server):
-    pkg_dir = devpi_server.workspace / 'pkg'
+    pkg_dir = devpi_server.workspace / "pkg"
     pkg_dir.mkdir_p()
-    setup_py = pkg_dir / 'setup.py'
-    setup_py.write_text("""
+    setup_py = pkg_dir / "setup.py"
+    setup_py.write_text(
+        """
 from setuptools import setup
 setup(name='test-foo',
       version='1.2.3')
-""")
+"""
+    )
     pkg_dir.chdir()
-    devpi_server.api('upload')
-    res = devpi_server.api('getjson', '/{}/{}'.format(devpi_server.user, devpi_server.index))
-    assert json.loads(res)['result']['projects'] == ['test-foo']
+    devpi_server.api("upload")
+    res = devpi_server.api(
+        "getjson", "/{}/{}".format(devpi_server.user, devpi_server.index)
+    )
+    assert json.loads(res)["result"]["projects"] == ["test-foo"]
 
 
 def test_function_index(devpi_server, devpi_function_index):
-    res = devpi_server.api('getjson', '/{}/test_function_index'.format(devpi_server.user))
+    res = devpi_server.api(
+        "getjson", "/{}/test_function_index".format(devpi_server.user)
+    )
     assert json.loads(res) == NEW_INDEX
-
