fcf593a55a2235a20a442a22696f6f39fe089e6c
Edward J. M. Easton <eeaston@gmail.com>
2019-12-08 12:40:18 +0000

    Added pytest-profiling to windows build

--- pytest-profiling/setup.py
+++ pytest-profiling/setup.py
@@ -13,6 +13,7 @@ classifiers = [
     'Topic :: Utilities',
     'Intended Audience :: Developers',
     'Operating System :: POSIX',
+    'Operating System :: Microsoft :: Windows',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
 ]
