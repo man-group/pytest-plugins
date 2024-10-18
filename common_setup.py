4e2bdd0f53069587d3f1cfacf3e0e9a108deb02a
jbannister <Jon.Bannister@man.com>
2024-10-18 22:41:38 +0100

    Remove usage of deprecated PyTest test command & add junit.xml to test-tmpl

--- common_setup.py
+++ common_setup.py
@@ -1,33 +1,5 @@
 # Common setup.py code shared between all the projects in this repository
-import sys
 import os
-import logging
-
-from setuptools.command.test import test as TestCommand
-from wheel.bdist_wheel import bdist_wheel
-
-
-class PyTest(TestCommand):
-    pytest_args = []
-    src_dir = None
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s %(message)s', level='DEBUG')
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        self.pytest_args.extend(['--junitxml', 'junit.xml'])
-        logger = logging.getLogger(__name__)
-        logger.info("Pytest args are {}".format(str(PyTest.pytest_args)))
-        errno = pytest.main(PyTest.pytest_args)
-        sys.exit(errno)
 
 
 def common_setup(src_dir):
@@ -38,12 +10,6 @@ def common_setup(src_dir):
 
     long_description = open(readme_file).read()
     changelog = open(changelog_file).read()
-    # Gather trailing arguments for pytest, this can't be done using setuptools' api
-    if 'test' in sys.argv:
-        PyTest.pytest_args = sys.argv[sys.argv.index('test') + 1:]
-        if PyTest.pytest_args:
-            sys.argv = sys.argv[:-len(PyTest.pytest_args)]
-    PyTest.src_dir = src_dir
 
     return dict(
         # Version is shared between all the projects in this repo
@@ -53,7 +19,6 @@ def common_setup(src_dir):
         url='https://github.com/man-group/pytest-plugins',
         license='MIT license',
         platforms=['unix', 'linux'],
-        cmdclass={'test': PyTest, 'bdist_wheel': bdist_wheel},
         include_package_data=True,
         python_requires='>=3.6',
     )
