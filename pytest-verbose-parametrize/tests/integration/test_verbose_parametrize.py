6c6736e954ee449865a9b233b8de015429b94db2
jbannister <Jon.Bannister@man.com>
2024-10-19 21:20:39 +0100

    Pytest<8 has the fully qualified name in the output of --collectonly

--- pytest-verbose-parametrize/tests/integration/test_verbose_parametrize.py
+++ pytest-verbose-parametrize/tests/integration/test_verbose_parametrize.py
@@ -1,18 +1,19 @@
 import sys
 import os
 
-from pkg_resources import resource_filename  # @UnresolvedImport
+from pkg_resources import resource_filename, get_distribution  # @UnresolvedImport
 from pytest_shutil.run import run_with_coverage
 
 TEST_DIR = resource_filename('pytest_verbose_parametrize', 'tests/integration/parametrize_ids')
-PYTEST = os.path.join(os.path.dirname(sys.executable), 'py.test')
-
+PYTEST = os.path.join(os.path.dirname(sys.executable), 'pytest')
+PYTEST_VERSION = get_distribution("pytest").parsed_version
+MODULE_PREFIX = "" if PYTEST_VERSION.major >= 8 else "tests/integration/parametrize_ids/tests/unit/"
 
 
 def test_parametrize_ids_generates_ids(pytestconfig):
     output = run_with_coverage([PYTEST, '--collectonly', 'tests/unit/test_parametrized.py'],
                                 pytestconfig, cd=TEST_DIR)
-    expected_lines = ["<Module test_parametrized.py", "<Function test_foo[sum-list]>", "<Function test_foo[len-int]>"]
+    expected_lines = [f"<Module {MODULE_PREFIX}test_parametrized.py", "<Function test_foo[sum-list]>", "<Function test_foo[len-int]>"]
     for line in expected_lines:
         assert line in output
 
@@ -20,7 +21,7 @@ def test_parametrize_ids_generates_ids(pytestconfig):
 def test_parametrize_ids_leaves_nonparametrized(pytestconfig):
     output = run_with_coverage([PYTEST, '--collectonly', 'tests/unit/test_non_parametrized.py'],
                                 pytestconfig, cd=TEST_DIR)
-    expected_lines = ["<Module test_non_parametrized.py>", "<Function test_bar>"]
+    expected_lines = [f"<Module {MODULE_PREFIX}test_non_parametrized.py>", "<Function test_bar>"]
     for line in expected_lines:
         assert line in output
 
@@ -28,7 +29,7 @@ def test_parametrize_ids_leaves_nonparametrized(pytestconfig):
 def test_handles_apparent_duplicates(pytestconfig):
     output = run_with_coverage([PYTEST, '--collectonly', 'tests/unit/test_duplicates.py'],
                                 pytestconfig, cd=TEST_DIR)
-    expected = '''<Module test_duplicates.py>
+    expected = f'''<Module {MODULE_PREFIX}test_duplicates.py>
 <Function test_foo[0-[1]]>
 <Function test_foo[0-[1]#1]>
 <Function test_foo[0-[1]#2]>
@@ -41,7 +42,7 @@ def test_handles_apparent_duplicates(pytestconfig):
 def test_truncates_long_ids(pytestconfig):
     output = run_with_coverage([PYTEST, '--collectonly', 'tests/unit/test_long_ids.py'],
                                pytestconfig, cd=TEST_DIR)
-    expected = '''<Module test_long_ids.py>
+    expected = f'''<Module {MODULE_PREFIX}test_long_ids.py>
 <Function test_foo[[0, 1, 2, 3, 4, 5, 6, 7, 8, 9...-None]>
 '''
     expected_lines = expected.splitlines()
